0ce6e02ebc705c6784a5aca461a2f8b9b59b35ee
EuDanzin1 <65622154+EuDanzin1@users.noreply.github.com>
2022-09-27 20:48:00 -0300

    Update PessoaLegal.java

--- Exercício6/PessoaLegal.java
+++ Exercício6/PessoaLegal.java
@@ -1,1 +1,20 @@
+package exercicio6;
+
+public class PessoaLegal extends Pessoa {
+	private String cnpj;
+	
+	public PessoaLegal(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	@Override
+	public boolean isValid() {
+		return ValidacaoCNPJ.isCNPJ(cnpj);
+	}
+	@Override
+    public String toString() {
+        return String.format("{Legal}, CNPJ: %s", this.cnpj);
+    }
+
+}
 
