5c5baa63f6d44f03d209764058aa426ca2eb390d
EuDanzin1 <65622154+EuDanzin1@users.noreply.github.com>
2022-09-27 20:45:43 -0300

    Update Main.java

--- Exercício6/Main.java
+++ Exercício6/Main.java
@@ -1,1 +1,18 @@
+package exercicio6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PessoaIndividual Pi1 = new PessoaIndividual("12834462437");
+		PessoaLegal Pl1 = new PessoaLegal("11472307000101");
+		PessoaIndividual Pi2 = new PessoaIndividual("12834462438");
+		PessoaLegal Pl2 = new PessoaLegal("12472307000101");
+	    System.out.println(Pi1.isValid());
+	    System.out.println(Pl1.isValid());
+	    System.out.println(Pi2.isValid());
+	    System.out.println(Pl2.isValid());
+	
+	}
+
+}
 
