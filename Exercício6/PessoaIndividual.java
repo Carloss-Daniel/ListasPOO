842cef3e4cc437dfb31528b4854254a8e5706eb0
EuDanzin1 <65622154+EuDanzin1@users.noreply.github.com>
2022-09-27 20:47:14 -0300

    Update PessoaIndividual.java

--- Exercício6/PessoaIndividual.java
+++ Exercício6/PessoaIndividual.java
@@ -1,1 +1,18 @@
+package exercicio6;
+
+public class PessoaIndividual extends Pessoa {
+	private String cpf;
+	
+	public PessoaIndividual(String cpf) {
+		this.cpf = cpf;
+	}
+	@Override
+	public boolean isValid() {
+		return ValidacaoCPF.isCPF(cpf);
+	}
+	 @Override
+	 public String toString() {
+		 return String.format("{Individual}, CPF: %s", this.cpf);
+	 }
+}
 
